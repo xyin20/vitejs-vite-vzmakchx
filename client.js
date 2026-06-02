517cf5f81f54d797640bca31a5d825da3d3b3732
xyin <xyin20@asu.edu>
2026-06-01 23:13:45 -0700

    Delete client.js

--- client.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const URL = 'https://invieufgokbnuwidvjsw.supabase.co'
-const API_KEY = 'sb_publishable_zFCygug6yr20L8EjkrgcDQ_hq8PBB5S'
-
-export const supabase = createClient(URL, API_KEY)
